9910c963faf20c996292c67d7ecb978fdc5e0946
Jakub Cepielik <jakubcepielik168@gmail.com>
2025-08-01 20:30:35 +0200

    usuniecie starych plików

--- backend/AIO_API/obj/Debug/net6.0/AIO_API.AssemblyInfo.cs
+++ backend/AIO_API/obj/Debug/net6.0/AIO_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIO_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5d2ce77d864c069c691c0ef714c5f4a8e107a7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aeee589259191bcb68093cdb1ddd24adf4c55eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIO_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIO_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
